7dfc3c594cafd5e33b82bbb1fe14f8c9939643e6
sean.brackenborough <sean.brackenborough@sysmech.co.uk>
2024-07-15 22:30:44 +0100

    populate todos with response from api

--- src/lib/Todos.svelte
+++ src/lib/Todos.svelte
@@ -67,7 +67,7 @@
           <div class="card-body py-4 px-4 px-md-5">
             <p class="h1 text-center mt-3 mb-4 pb-3 text-primary">
               <i class="fas fa-check-square me-1"></i>
-              <u>My Todo-s</u>
+              <u>Todos</u>
             </p>
 
             <div class="pb-2">
@@ -80,16 +80,16 @@
                       id="exampleFormControlInput1"
                       placeholder="Add new..."
                     />
-                    <a href="#!" data-mdb-tooltip-init title="Set due date"
-                      ><i class="fas fa-calendar-alt fa-lg me-3"></i></a
-                    >
                     <div>
                       <button
                         type="button"
                         data-mdb-button-init
                         data-mdb-ripple-init
-                        class="btn btn-primary">Add</button
+                        class="btn btn-primary"
+                        on:click={createTodo}
                       >
+                        Add
+                      </button>
                     </div>
                   </div>
                 </div>
@@ -121,193 +121,69 @@
                 ><i class="fas fa-sort-amount-down-alt ms-2"></i></a
               >
             </div>
-
-            <ul
-              class="list-group list-group-horizontal rounded-0 bg-transparent"
-            >
-              <li
-                class="list-group-item d-flex align-items-center ps-0 pe-3 py-1 rounded-0 border-0 bg-transparent"
+            {#each todos as todo (todo.id)}
+              <!-- TODO implement deleting & completing todos -->
+              <ul
+                class="list-group list-group-horizontal rounded-0 bg-transparent"
               >
-                <div class="form-check">
-                  <input
-                    class="form-check-input me-0"
-                    type="checkbox"
-                    value=""
-                    id="flexCheckChecked1"
-                    aria-label="..."
-                    checked
-                  />
-                </div>
-              </li>
-              <li
-                class="list-group-item px-3 py-1 d-flex align-items-center flex-grow-1 border-0 bg-transparent"
-              >
-                <p class="lead fw-normal mb-0">Buy groceries for next week</p>
-              </li>
-              <li
-                class="list-group-item ps-3 pe-0 py-1 rounded-0 border-0 bg-transparent"
-              >
-                <div class="d-flex flex-row justify-content-end mb-1">
-                  <a
-                    href="#!"
-                    class="text-info"
-                    data-mdb-tooltip-init
-                    title="Edit todo"><i class="fas fa-pencil-alt me-3"></i></a
-                  >
-                  <a
-                    href="#!"
-                    class="text-danger"
-                    data-mdb-tooltip-init
-                    title="Delete todo"><i class="fas fa-trash-alt"></i></a
-                  >
-                </div>
-                <div class="text-end text-muted">
-                  <a
-                    href="#!"
-                    class="text-muted"
-                    data-mdb-tooltip-init
-                    title="Created date"
-                  >
-                    <p class="small mb-0">
-                      <i class="fas fa-info-circle me-2"></i>28th Jun 2020
-                    </p>
-                  </a>
-                </div>
-              </li>
-            </ul>
-            <ul class="list-group list-group-horizontal rounded-0">
-              <li
-                class="list-group-item d-flex align-items-center ps-0 pe-3 py-1 rounded-0 border-0 bg-transparent"
-              >
-                <div class="form-check">
-                  <input
-                    class="form-check-input me-0"
-                    type="checkbox"
-                    value=""
-                    id="flexCheckChecked2"
-                    aria-label="..."
-                  />
-                </div>
-              </li>
-              <li
-                class="list-group-item px-3 py-1 d-flex align-items-center flex-grow-1 border-0 bg-transparent"
-              >
-                <p class="lead fw-normal mb-0">Renew car insurance</p>
-              </li>
-              <li
-                class="list-group-item px-3 py-1 d-flex align-items-center border-0 bg-transparent"
-              >
-                <div
-                  class="py-2 px-3 me-2 border border-warning rounded-3 d-flex align-items-center bg-body-tertiary"
+                <li
+                  class="list-group-item d-flex align-items-center ps-0 pe-3 py-1 rounded-0 border-0 bg-transparent"
                 >
-                  <p class="small mb-0">
-                    <a href="#!" data-mdb-tooltip-init title="Due on date">
-                      <i class="fas fa-hourglass-half me-2 text-warning"></i>
+                  <div class="form-check">
+                    <input
+                      class="form-check-input me-0"
+                      type="checkbox"
+                      value=""
+                      id="flexCheckChecked1"
+                      aria-label="..."
+                      checked
+                    />
+                  </div>
+                </li>
+                <li
+                  class="list-group-item px-3 py-1 d-flex align-items-center flex-grow-1 border-0 bg-transparent"
+                >
+                  <p class="lead fw-normal mb-0">{todo.name}</p>
+                </li>
+                <li
+                  class="list-group-item ps-3 pe-0 py-1 rounded-0 border-0 bg-transparent"
+                >
+                  <div class="d-flex flex-row justify-content-end mb-1">
+                    <a
+                      href="#!"
+                      class="text-info"
+                      data-mdb-tooltip-init
+                      title="Edit todo"
+                      ><i class="fas fa-pencil-alt me-3"></i></a
+                    >
+                    <a
+                      href="#!"
+                      class="text-danger"
+                      data-mdb-tooltip-init
+                      title="Delete todo"><i class="fas fa-trash-alt"></i></a
+                    >
+                  </div>
+                  <div class="text-end text-muted">
+                    <a
+                      href="#!"
+                      class="text-muted"
+                      data-mdb-tooltip-init
+                      title="Created date"
+                    >
+                      <p class="small mb-0">
+                        <i class="fas fa-info-circle me-2"></i>28th Jun 2020
+                      </p>
                     </a>
-                    28th Jun 2020
-                  </p>
-                </div>
-              </li>
-              <li
-                class="list-group-item ps-3 pe-0 py-1 rounded-0 border-0 bg-transparent"
-              >
-                <div class="d-flex flex-row justify-content-end mb-1">
-                  <a
-                    href="#!"
-                    class="text-info"
-                    data-mdb-tooltip-init
-                    title="Edit todo"><i class="fas fa-pencil-alt me-3"></i></a
-                  >
-                  <a
-                    href="#!"
-                    class="text-danger"
-                    data-mdb-tooltip-init
-                    title="Delete todo"><i class="fas fa-trash-alt"></i></a
-                  >
-                </div>
-                <div class="text-end text-muted">
-                  <a
-                    href="#!"
-                    class="text-muted"
-                    data-mdb-tooltip-init
-                    title="Created date"
-                  >
-                    <p class="small mb-0">
-                      <i class="fas fa-info-circle me-2"></i>28th Jun 2020
-                    </p>
-                  </a>
-                </div>
-              </li>
-            </ul>
-            <ul class="list-group list-group-horizontal rounded-0 mb-2">
-              <li
-                class="list-group-item d-flex align-items-center ps-0 pe-3 py-1 rounded-0 border-0 bg-transparent"
-              >
-                <div class="form-check">
-                  <input
-                    class="form-check-input me-0"
-                    type="checkbox"
-                    value=""
-                    id="flexCheckChecked3"
-                    aria-label="..."
-                  />
-                </div>
-              </li>
-              <li
-                class="list-group-item px-3 py-1 d-flex align-items-center flex-grow-1 border-0 bg-transparent"
-              >
-                <p
-                  class="lead fw-normal mb-0 bg-body-tertiary w-100 ms-n2 ps-2 py-1 rounded"
-                >
-                  Sign up for online course
-                </p>
-              </li>
-              <li
-                class="list-group-item ps-3 pe-0 py-1 rounded-0 border-0 bg-transparent"
-              >
-                <div class="d-flex flex-row justify-content-end mb-1">
-                  <a
-                    href="#!"
-                    class="text-danger"
-                    data-mdb-tooltip-init
-                    title="Delete todo"><i class="fas fa-trash-alt"></i></a
-                  >
-                </div>
-                <div class="text-end text-muted">
-                  <a
-                    href="#!"
-                    class="text-muted"
-                    data-mdb-tooltip-init
-                    title="Created date"
-                  >
-                    <p class="small mb-0">
-                      <i class="fas fa-info-circle me-2"></i>28th Jun 2020
-                    </p>
-                  </a>
-                </div>
-              </li>
-            </ul>
+                  </div>
+                </li>
+              </ul>
+            {/each}
           </div>
         </div>
       </div>
     </div>
   </div>
 </section>
-
-<div id="todos">
-  <h1>Todo App</h1>
-  <div>
-    <input type="text" placeholder="Add a new todo" />
-    <button on:click={createTodo}>Add Todo</button>
-    <!--// TODO implement this -->
-  </div>
-  <ul>
-    {#each todos as todo (todo.id)}
-      <li>{todo.name}</li>
-      <!-- TODO implement deleting & completing todos -->
-    {/each}
-  </ul>
-</div>
 
 <style>
   #list1 .form-control {
