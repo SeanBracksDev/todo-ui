21d0374bf907035c2c2d76290e3f571f17b65c06
sean.brackenborough <sean.brackenborough@sysmech.co.uk>
2024-07-16 21:25:28 +0100

    refactor CRUD functions and improve API integration

--- src/lib/crud.ts
+++ src/lib/crud.ts
@@ -2,7 +2,7 @@ export interface TodoType {
   id: number;
   name: string;
   is_done: boolean;
-  created_at: Date;
+  created_at: string;
 }
 
 export const todoApiUrl = "http://127.0.0.1:8000";
@@ -17,7 +17,7 @@ export async function getTodos(): Promise<TodoType[]> {
   return todos as TodoType[];
 }
 
-export async function postTodo() {
+export async function postTodo(todos: TodoType[]): Promise<TodoType[]> {
   const headers: Headers = new Headers();
   headers.set("Content-Type", "application/json");
   headers.set("Accept", "application/json");
@@ -25,7 +25,7 @@ export async function postTodo() {
   let todo_name = (document.querySelector("input") as HTMLInputElement).value;
 
   if (!todo_name) {
-    return;
+    return todos;
   }
 
   let todo = {
@@ -38,8 +38,36 @@ export async function postTodo() {
     body: JSON.stringify(todo),
   });
 
-  return fetch(request).then(async (res) => {
+  fetch(request).then(async (res) => {
     const created_todo = (await res.json()) as TodoType;
     todos = [...todos, created_todo];
   });
+  return todos;
+}
+
+export async function deleteTodo(id: number) {
+  const headers: Headers = new Headers();
+  headers.set("Content-Type", "application/json");
+  headers.set("Accept", "application/json");
+
+  const request: RequestInfo = new Request(`${todoApiUrl}/todos/${id}`, {
+    method: "DELETE",
+    headers: headers,
+  });
+
+  fetch(request);
+}
+
+export async function toggleTodo(todo: TodoType) {
+  const headers: Headers = new Headers();
+  headers.set("Content-Type", "application/json");
+  headers.set("Accept", "application/json");
+
+  const request: RequestInfo = new Request(`${todoApiUrl}/todos/${todo.id}`, {
+    method: "PATCH",
+    headers: headers,
+    body: JSON.stringify({ is_done: !todo.is_done }),
+  });
+
+  fetch(request);
 }
